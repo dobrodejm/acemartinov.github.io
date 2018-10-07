4845a363a4b77d7d05d8d715e458ba3995db1f64
Ace Martinov <acemartinov@outlook.com>
2018-10-07 19:36:28 +0200

    js notes v1

--- timbre/sound.js
+++ timbre/sound.js
@@ -1,9 +1,20 @@
-
 //variables
 var apply = document.getElementById("apply"); // the actual project thing that does the sound
 //these two are here for the whole thing to start up
 var user_volume = document.getElementById("user_volume").value;
 var user_wave = document.getElementById("user_waveform").value;
+//note arrays
+var notes = {
+oct0 : [16.35,17.32,18.35,19.45,20.60,21.83,23.12,24.50,25.96,27.50,29.14,30.87],
+oct1 : [32.70,34.65,36.71,38.89,41.20,43.65,46.25,490,51.91,550,58.27,61.74],
+oct2 : [65.41,69.30,73.42,77.78,82.41,87.31,92.50,980,103.8,110,116.5,123.5],
+oct3 : [130.8,138.6,146.8,155.6,164.8,174.6,185,196,207.7,220,233.1,246.9],
+oct4 : [261.6,277.2,293.7,311.1,329.6,349.2,370,392,415.3,440,466.2,493.9],
+oct5 : [523.3,554.4,587.3,622.3,659.3,698.5,740,784,830.6,880,932.3,987.8],
+oct6 : [1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,1865,1976],
+oct7 : [2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951],
+oct8 : [4186,4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902]
+}
 
 // make sure user input is updated
 document.getElementById("user_volume").addEventListener('change', function() {
@@ -61,6 +72,7 @@ function keyDownTextField(e) {
 function playsound() {
 // get user input
 let user_freq = document.getElementById("user_freq").value;
+
 let user_wave = document.getElementById("user_waveform").value;
 // make noise
 let context = new (window.AudioContext || window.webkitAudioContext)();
